be26c024a588db92584e356e5de3766ff91c86d2
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2016-03-15 15:36:04 -0700

    Standardize formatting in lmc.js

--- lmc.js
+++ lmc.js
@@ -1,35 +1,27 @@
 jQuery(document).ready(function($) {
-  
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-  var href_value = e.target.toString();
-  //alert(href_value);
-  var split_href_array = href_value.split('/');
- 
-  var last_element = split_href_array[split_href_array.length -1];
-  //alert(last_element);
-  var hidden_tab_el = $('#hidden_tab');
- switch(last_element)
- {
-    case '#search':
-        hidden_tab_el.val('default_tab');
-         break;
-    case '#books':
-        hidden_tab_el.val('books');
-         break;
-    case '#articles':
-        hidden_tab_el.val('articles');
-         break;
-    case '#course':
-        hidden_tab_el.val('cr');
-         break;
-         
- }
- 
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+		var href_value = e.target.toString();
+		var split_href_array = href_value.split('/');
+		var last_element = split_href_array[split_href_array.length -1];
+ 		var hidden_tab_el = $('#hidden_tab');
+		switch(last_element) {
+			case '#search':
+				hidden_tab_el.val('default_tab');
+				break;
+			case '#books':
+				hidden_tab_el.val('books');
+				break;
+			case '#articles':
+				hidden_tab_el.val('articles');
+				break;
+			case '#course':
+				hidden_tab_el.val('cr');
+				break;
+		}
+	});
 });
-    
-   
- });
-  function searchPrimo() { 
+
+function searchPrimo() {
 	document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value.replace(/[,]/g, " ");
-        document.forms["searchForm"].submit();
-    }
+	document.forms["searchForm"].submit();
+}
